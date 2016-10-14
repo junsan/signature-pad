4086ed0445d59f047fcd0a2a97a6fd08dc88ef20
xjun13 <xjunsantos@gmail.com>
2016-10-14 21:52:13 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component }              from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  public title = 'Sample Contract';
+  private form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+
+    this.form = fb.group({
+      signatureField: '',
+    });
+  }
+
+}
